74170a3772660971270f8ce6e4d7b4d910c9644c
Richard Pope <richard@memespring.co.uk>
2014-07-04 09:58:27 +0100

    fdsfds

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask, request, redirect, render_template
 import jinja2
 import os
+from identifiers import *
+import govuk
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def index():
+
     return render_template('index.html')
+
+@app.route("/search", methods=['POST'])
+def search():
+
+    search_term = request.form.get('q', None)
+    search_type = 'unknown'
+    standard_results = []
+
+    if is_postcode(search_term):
+        search_type = 'postcode'
+    elif is_land_title(search_term):
+        search_type = 'land registry title'
+    elif is_numberplate(search_term):
+        search_type = 'vehicle'
+    elif is_unique_tax_reference(search_term):
+        search_type = 'tax reference number'
+        standard_results = govuk.search('Self Assessment')
+    elif is_national_insurance_number(search_term):
+        search_type = 'national insurance number'
+    elif is_passport_number(search_term):
+        search_type = 'passport number'
+        standard_results = govuk.search('passport')
+
+    return render_template('results.html', search_type=search_type, standard_results=standard_results)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
---
+++ identifiers.py
@@ -0,0 +1,65 @@
+import re
+
+def is_national_insurance_number(subject):
+    pattern = "^\s*[a-zA-Z]{2}(?:\s*\d\s*){6}[a-zA-Z]?\s*$"
+    if re.match(pattern, subject):
+        return True
+    else:
+        return False
+
+def is_unique_tax_reference(subject):
+    pattern = "^[0-9]{10}$"
+    if re.match(pattern, subject):
+        return True
+    else:
+        return False
+
+def is_passport_number(subject):
+    pattern = "^[0-9]{9}$"
+    if re.match(pattern, subject):
+        return True
+    else:
+        return False
+
+def is_land_title(subject):
+    pattern = "^(AGL|BD|BGL|BK|BL|BM|CB|CE|CH|CL|CU|CYM|DN|DT|DU|DY|ESX|EX|GR|HD|HE|HP|HS|IW|K|LAN|LL|LT|MAN|MM|MS|ND|NGL|NK|NN|NT|NYK|ON|PM|SF|SGL|SH|SK|SL|ST|SY|SYK|TGL|TT|TY|WK|WR|WS|WSX|WT|YEA|YY)[0-9]*$"
+    if re.match(pattern, subject):
+        return True
+    else:
+        return False
+
+def is_numberplate(subject):
+    pattern = '^([A-Z]{3}\s?(\d{3}|\d{2}|d{1})\s?[A-Z])|([A-Z]\s?(\d{3}|\d{2}|\d{1})\s?[A-Z]{3})|(([A-HK-PRSVWY][A-HJ-PR-Y])\s?([0][2-9]|[1-9][0-9])\s?[A-HJ-PR-Z]{3})$'
+
+    if re.match(pattern, subject):
+        return True
+    else:
+        return False
+
+def is_postcode(postcode):
+
+    #See http://www.govtalk.gov.uk/gdsc/html/noframes/PostCode-2-1-Release.htm
+
+    in_ = 'ABDEFGHJLNPQRSTUWXYZ';
+    fst = 'ABCDEFGHIJKLMNOPRSTUWYZ';
+    sec = 'ABCDEFGHJKLMNOPQRSTUVWXY';
+    thd = 'ABCDEFGHJKSTUW';
+    fth = 'ABEHMNPRVWXY';
+    num = '0123456789';
+    nom = '0123456789';
+    gap = '\s\.';
+
+    if re.findall("^[%s][%s][%s]*[%s][%s][%s]$" % (fst, num, gap, nom, in_, in_), postcode, re.IGNORECASE):
+      return True
+    elif re.findall("^[%s][%s][%s][%s]*[%s][%s][%s]$" % (fst, num, num, gap, nom, in_, in_), postcode, re.IGNORECASE):
+      return True
+    elif re.findall("^[%s][%s][%s][%s]*[%s][%s][%s]$" % (fst, sec, num, gap, nom, in_, in_), postcode, re.IGNORECASE):
+      return True
+    elif re.findall("^[%s][%s][%s][%s][%s]*[%s][%s][%s]$" % (fst, sec, num, num, gap, nom, in_, in_), postcode, re.IGNORECASE):
+      return True
+    elif re.findall("^[%s][%s][%s][%s]*[%s][%s][%s]$" % (fst, num, thd, gap, nom, in_, in_), postcode, re.IGNORECASE):
+      return True
+    elif re.findall("^[%s][%s][%s][%s][%s]*[%s][%s][%s]$" % (fst, sec, num, fth, gap, nom, in_, in_), postcode, re.IGNORECASE):
+      return True
+    else:
+      return False
